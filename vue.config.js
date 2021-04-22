49592d3f02c6faa4b4a102aa9625dd83820e7016
CY-Pan <adredpcy@qq.com>
2021-04-22 18:29:05 +0800

    从upstream合并

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,12 @@
 module.exports = {
-<<<<<<< HEAD
 	devServer: {
 		proxy: {
 			'/api': {
 				target: 'http://121.41.94.85:5000/',
-				/*pathRewrite: {
-				  '^/api': '/'
-				}*/
+			},
+			'/static': {
+				target: './',
 			}
 		},
 	}
-=======
-  devServer: {
-    proxy: {
-	  '/api': {
-	    target: 'http://121.41.94.85:5000/',
-	  },
-	  '/static': {
-	    target: './',
-	  }
-    },
-  }
->>>>>>> origin/main
 }
