30a33a94f680c7480bd8ed3759c68488f08c0476
CY-Pan <adredpcy@qq.com>
2021-04-24 23:43:26 +0800

    改代理地址

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	devServer: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:5000/',
+				target: 'http://121.41.94.85:5000/',
 			},
 			'/static': {
 				target: './', 
